bf4517b8335ed9ce5b293905f82895226e132221
Eraldo Forgoli <eraldoforgoli@gmail.com>
2019-07-14 12:54:07 +0200

    refactor | changed code structure and unneccessary code

---
+++ src/controller/InputController.java
@@ -0,0 +1,57 @@
+package Controllers;
+
+import Models.Process;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class InputController {
+    private ArrayList<Process> allProcesses;
+    private int processNumber;
+    private int processPriority;
+    private int processArrivingTime;
+    private int processBurstTime;
+    private Scanner in;
+
+    public InputController() {
+        allProcesses = new ArrayList<Process>();
+        processNumber = 0;
+        processPriority = 0;
+        processArrivingTime = 0;
+        processBurstTime = 0;
+        in = new Scanner(System.in);
+    }
+
+    public ArrayList<Process> getAllProcesses() {
+
+        System.out.println("Start entering your processes: ");
+
+        do {
+            System.out.println("Enter the process Priority:  ");
+
+            while (!in.hasNextInt()) {
+                System.out.println("Wrong input, enter the Priority again as integer!");
+                in.next();
+            }
+            processPriority = in.nextInt();
+
+            System.out.println("Enter the process Arriving Time:   ");
+            while (!in.hasNextInt()) {
+                System.out.println("Wrong input, enter the Arriving Time again as integer!");
+                in.next();
+            }
+            processArrivingTime = in.nextInt();
+
+            System.out.println("Enter the process Burst Time:  ");
+            while (!in.hasNextInt()) {
+                in.next();
+                System.out.println("Wrong input, enter the Burst Time again as integer!");
+            }
+            processBurstTime = in.nextInt();
+
+            allProcesses.add(new Process(++processNumber, processPriority, processArrivingTime, processBurstTime));
+            System.out.println("Do You want to finish entering the processes? (y/n)");
+        } while (!(in.nextLine().equals("Y") || in.nextLine().equals("y")));
+
+        return allProcesses;
+    }
+}
---
+++ src/model/Process.java
@@ -0,0 +1,55 @@
+package Models;
+
+public class Process {
+
+    private int processNumber;
+    private int processPriority;
+    private int arrivingTime;
+    private int burstTime;
+
+    public Process() {
+        this.processNumber = 0;
+        this.processPriority = 0;
+        this.arrivingTime = 0;
+        this.burstTime = 0;
+    }
+
+    public Process(int processNumber, int processPriority, int arrivingTime, int burstTime) {
+        this.processNumber = processNumber;
+        this.processPriority = processPriority;
+        this.arrivingTime = arrivingTime;
+        this.burstTime = burstTime;
+    }
+
+    public int getProcessNumber() {
+        return processNumber;
+    }
+
+    public void setProcessNumber(int processNumber) {
+        this.processNumber = processNumber;
+    }
+
+    public int getProcessPriority() {
+        return processPriority;
+    }
+
+    public void setProcessPriority(int processPriority) {
+        this.processPriority = processPriority;
+    }
+
+    public int getArrivingTime() {
+        return arrivingTime;
+    }
+
+    public void setArrivingTime(int arrivingTime) {
+        this.arrivingTime = arrivingTime;
+    }
+
+    public int getBurstTime() {
+        return burstTime;
+    }
+
+    public void setBurstTime(int burstTime) {
+        this.burstTime = burstTime;
+    }
+}
